0c016a8c0415b9d1c380ebf8d21f1b5966eb74c8
19DennisBrown <densmoney12@gmail.com>
2024-04-11 10:52:17 +0300

    Debug true

--- LenderApp/settings.py
+++ LenderApp/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uy_ghyum529rupm9hj*(8wo-(h6(xh=v2t@x0zxqo(@enk8fru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
